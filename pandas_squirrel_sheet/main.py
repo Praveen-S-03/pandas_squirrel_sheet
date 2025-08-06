64e37748a878907b5b75c22404155316800b0b3b
Praveen S <connectwithpraveensuresh@gmail.com>
2025-08-06 13:14:29 +0530

    Update main.py

--- pandas_squirrel_sheet/main.py
+++ pandas_squirrel_sheet/main.py
@@ -1,5 +1,6 @@
 import pandas
-
+import matplotlib.pyplot as plt
+#------------------Pandas Workspace ------------------#
 data= pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250315.csv")
 gray_count = len(data[data["Primary Fur Color"]=="Gray"])
 red_count = len(data[data["Primary Fur Color"]=="Cinnamon"])
@@ -11,4 +12,13 @@ data_dic={
 }
 
 df = pandas.DataFrame(data_dic)
-ex = df.to_csv("S_count.csv")
+excel_output = df.to_csv("S_count.csv")
+#-----------------Graph plot ------------------------#
+plt.figure(figsize=(5,5))
+plt.bar(squirrel_color,squirrel_count)
+plt.title("Squirrel Count")
+plt.xlabel("Colour")
+plt.ylabel("Count")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
